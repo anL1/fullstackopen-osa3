4af6e68e7eb92db3ac4b377141167d0d13024587
Anssi Lintulampi <anssi.lintulampi@helsinki.fi>
2018-06-19 16:41:23 +0300

    heroku

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.get('/info', (req, res) => {
     <p>${new Date()}</p>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 })
